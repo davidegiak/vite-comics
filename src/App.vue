71a528d2d93868ce64773ab05d8208a210be1d70
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-23 16:02:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import HeaderApp from './components/HeaderApp.vue'
+</script>
+
+<template>
+  <HeaderApp/>
+</template>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,75 @@
+<script>
+    export default {
+        data() {
+            return {
+                links:[
+                    {
+                        text: "CHARACTERS",
+                        url: "#"
+                    },
+                    {
+                        text: "COMICS",
+                        url: "#"
+                    },
+                    {
+                        text: "MOVIES",
+                        url: "#"
+                    },
+                    {
+                        text: "TV",
+                        url: "#"
+                    },
+                    {
+                        text: "GAMES",
+                        url: "#"
+                    },
+                    {
+                        text: "COLLECTIBLES",
+                        url: "#"
+                    },
+                    {
+                        text: "VIDEOS",
+                        url: "#"
+                    },
+                    {
+                        text: "FANS",
+                        url: "#"
+                    },
+                    {
+                        text: "NEWS",
+                        url: "#"
+                    },
+                    {
+                        text: "SHOP",
+                        url: "#"
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <header class="d-flex justify-content-center align-items-center justify-content-around">
+        <div class="logo-cont d-flex justify-content-center">
+            <img class="my-2" src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="links-cont">
+            <a class="mx-2" v-for="link in links" :href="link.url">{{ link.text }}</a>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+    header{
+        background-color: white;
+        height: 6rem;
+    }
+    .logo-cont {
+        height: 100%;
+    }
+    img {
+        width: 100%;
+        object-fit: contain;
+    }
+</style>
